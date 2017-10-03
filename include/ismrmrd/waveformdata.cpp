e96902dbf5dd1536416bb178413a357b6797af0a
Morten Engell-Nørregård <morten.engell-norregard@alexandra.dk>
2017-10-03 11:05:51 +0200

    new files added

---
+++ include/ismrmrd/waveformdata.cpp
@@ -0,0 +1,19 @@
+#include "waveformdata.h"
+
+
+//constructor
+ISMRMRD::WaveformData::WaveformData()
+{
+	//make sure the containers are made and allocated here 
+}
+
+//destructor
+ISMRMRD::WaveformData::~WaveformData()
+{
+	//release 
+}
+
+void ISMRMRD::WaveformData::init()
+{
+
+}
---
+++ include/ismrmrd/waveformdata.h
@@ -0,0 +1,34 @@
+/* ISMRMRD Waveform  Data Set */
+
+/**
+ * @file waveformdata.h
+ */
+
+#pragma once
+#ifndef WAVEFORM_DATA_H
+#define WAVEFORM_DATA_H
+
+//Probably still needed
+#include "ismrmrd/ismrmrd.h"
+#include <hdf5.h>
+
+#ifdef __cplusplus
+#include <string>
+namespace ISMRMRD {
+#endif
+
+/**
+ *   A class for storing a dataset includig waveforms
+ *
+ */
+	class WaveformData 
+{
+	det er en fejl
+
+};
+
+
+} /* ISMRMRD namespace */
+#endif
+
+#endif /* WAVEFORM_DATA_H */
