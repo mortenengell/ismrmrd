773ffbd8348b495c46825bb36cafa5e3c882d0e4
Morten Engell-Nørregård <morten.engell-norregard@alexandra.dk>
2017-10-11 16:21:07 +0200

    fixed the false iterator initialisation

--- include/ismrmrd/mrdutil.h
+++ include/ismrmrd/mrdutil.h
@@ -9,7 +9,7 @@
 #include <vector>
 
 /**********
-* a helper function to calculate the size of the map in a wavedform header
+* a helper function to calculate the size of the map in a waveform header
 * @return 
 * @param mymap  // the map to be calulated
 ***********/
@@ -24,7 +24,7 @@
 	uint32_t map_size(std::map<uint32_t, std::vector<T> > mymap) {
 		uint32_t size = 0;
 
-		for (mymap::const_iterator it = mymap.begin(); it != mymap.end(); it++) {
+		for (std::map<uint32_t, std::vector<T> >::const_iterator it = mymap.begin(); it != mymap.end(); it++) {
 			size += sizeof(it->first);
 			size += (it->second).size() * sizeof(T);
 		}
@@ -35,7 +35,7 @@
 	uint32_t map_size(std::map<uint32_t, T > mymap) {
 		uint32_t size = 0;
 
-		for (mymap::const_iterator it = mymap.begin(); it != mymap.end(); it++) {
+		for (std::map<uint32_t, T >::const_iterator it = mymap.begin(); it != mymap.end(); it++) {
 			size += sizeof(it->first);
 			size += sizeof(T);
 		}
